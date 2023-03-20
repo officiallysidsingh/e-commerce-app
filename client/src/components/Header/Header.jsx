398edf27f96b994d3d95e836f0cd9311f7c44cbf
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-03-20 20:07:16 +0530

    added header section and styles for header section

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,6 +1,52 @@
+import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { TbSearch } from "react-icons/tb";
+import { CgShoppingCart } from "react-icons/cg";
+import { AiOutlineHeart } from "react-icons/ai";
+
+import Search from "./Search/Search";
+import Cart from "../Cart/Cart";
+import { Context } from "../../utils/context.jsx";
+
 import "./Header.scss";
+
 const Header = () => {
-    return <div>Header</div>;
+  const [scrolled, setScrolled] = useState(false);
+
+  const handleScroll = () => {
+    const offset = window.scrollY;
+    if (offset > 200) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <header className={`mainHeader ${scrolled ? "stickyHeader" : ""}`}>
+      <div className="headerContent">
+        <ul className="left">
+          <li>Home</li>
+          <li>About</li>
+          <li>Categories</li>
+        </ul>
+        <div className="center">SID-ECOM-STORE</div>
+        <div className="right">
+          <TbSearch />
+          <AiOutlineHeart />
+          <span className="cartIcon">
+            <CgShoppingCart />
+            <span>5</span>
+          </span>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
