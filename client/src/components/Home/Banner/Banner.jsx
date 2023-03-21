667ef3318b2ca5508c48d1f6f5cfc627c9caacf7
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-03-21 19:03:08 +0530

    added components to the banner section

--- client/src/components/Home/Banner/Banner.jsx
+++ client/src/components/Home/Banner/Banner.jsx
@@ -1,6 +1,26 @@
+import BannerImg from "../../../assets/banner-img.png";
+
 import "./Banner.scss";
+
 const Banner = () => {
-    return <div>Banner</div>;
+  return (
+    <div className="heroBanner">
+      <div className="content">
+        <div className="textContent">
+          <h1>SALES</h1>
+          <p>
+            Convallis interdum purus adipiscing dis parturient posuere ac a quam
+            a eleifend montes parturient posuere curae tempor
+          </p>
+          <div className="ctas">
+            <div className="bannerCta">Read More</div>
+            <div className="bannerCta v2">Shop Now</div>
+          </div>
+        </div>
+        <img className="bannerImg" src={BannerImg} alt="Banner Image" />
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
