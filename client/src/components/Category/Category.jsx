b655f9e19c71d73a8d7e20c1ac174f027f42042e
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-03-22 01:59:27 +0530

    added basic blueprint for category page

--- client/src/components/Category/Category.jsx
+++ client/src/components/Category/Category.jsx
@@ -1,6 +1,16 @@
+import Products from "../Products/Products";
+
 import "./Category.scss";
+
 const Category = () => {
-  return <div>Category</div>;
+  return (
+    <div className="categoryMainContent">
+      <div className="layout">
+        <div className="categoryTitle">Category Title</div>
+        <Products innerPage={true} />
+      </div>
+    </div>
+  );
 };
 
 export default Category;
