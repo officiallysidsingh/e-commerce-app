46d315f81db6b234e2a73442dc73736f91528a04
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-03-22 01:17:16 +0530

    added basic template for products section

--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -1,6 +1,23 @@
+import Product from "./Product/Product";
+
 import "./Products.scss";
+
 const Products = () => {
-    return <div>Products</div>;
+  return (
+    <div className="productsContainer">
+      <div className="sectionHeading">Section Heading</div>
+      <div className="products">
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </div>
+    </div>
+  );
 };
 
 export default Products;
