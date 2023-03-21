c7bdc375fb3af4b1666528b8ec8de9b6140c03f3
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-03-22 01:34:29 +0530

    added basic blueprint for each product card

--- client/src/components/Products/Product/Product.jsx
+++ client/src/components/Products/Product/Product.jsx
@@ -1,6 +1,19 @@
+import prod from "../../../assets/products/earbuds-prod-1.webp";
+
 import "./Product.scss";
+
 const Product = () => {
-    return <div>Product</div>;
+  return (
+    <div className="productCard">
+      <div className="thumbnail">
+        <img src={prod} alt="Product Image" />
+      </div>
+      <div className="productDetails">
+        <span className="name">Product Name</span>
+        <span className="price">&#8377;499</span>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
