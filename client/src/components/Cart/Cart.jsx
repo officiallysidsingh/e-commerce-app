b50392fa0e305d16e34eae08f4e701989708e0e9
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-03-23 22:47:15 +0530

    added cart component basic blueprint

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,6 +1,42 @@
+import CartItem from "./CartItem/CartItem";
+import { MdClose } from "react-icons/md";
+import { BsCartX } from "react-icons/bs";
+
 import "./Cart.scss";
-const Cart = () => {
-    return <div>Cart</div>;
+
+const Cart = ({ setShowCart }) => {
+  return (
+    <div className="cartPanel">
+      <div className="opacLayer"></div>
+      <div className="cartContent">
+        <div className="cartHeader">
+          <span className="heading">Shopping Cart</span>
+          <span className="closeBtn" onClick={() => setShowCart(false)}>
+            <MdClose />
+            <span className="text">Close</span>
+          </span>
+        </div>
+
+        {/*<div className="emptyCart">
+          <BsCartX />
+          <span>No products in the cart.</span>
+          <button className="returnCta">RETURN TO SHOP</button>
+        </div>*/}
+        <>
+          <CartItem />
+          <div className="cartFooter">
+            <div className="subtotal">
+              <span className="text">Subtotal: </span>
+              <span className="text total">&#8377;1234</span>
+            </div>
+            <div className="button">
+              <button className="checkoutCta">Checkout</button>
+            </div>
+          </div>
+        </>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
