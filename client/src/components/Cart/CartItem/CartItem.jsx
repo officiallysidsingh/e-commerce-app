5a882e766a1c94d2551f3779b6c64ebc04b6f83a
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-03-23 23:02:50 +0530

    added basic blueprint of cart item

--- client/src/components/Cart/CartItem/CartItem.jsx
+++ client/src/components/Cart/CartItem/CartItem.jsx
@@ -1,6 +1,32 @@
+import { MdClose } from "react-icons/md";
+import prod from "../../../assets/products/earbuds-prod-1.webp";
+
 import "./CartItem.scss";
+
 const CartItem = () => {
-    return <div>Cart item</div>;
+  return (
+    <div className="cartProducts">
+      <div className="cartProduct">
+        <div className="imgContainer">
+          <img src={prod} alt="product" />
+        </div>
+        <div className="prodDetails">
+          <span className="name">Product Name</span>
+          <MdClose className="closeBtn" />
+          <div className="quantityButtons">
+            <span>-</span>
+            <span>5</span>
+            <span>+</span>
+          </div>
+          <div className="text">
+            <span>3</span>
+            <span>x</span>
+            <span className="highlight">&#8377;4567</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CartItem;
