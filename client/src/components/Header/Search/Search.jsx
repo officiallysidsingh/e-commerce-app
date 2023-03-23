f12c603d9d10105dea56f4c612b40586c2452358
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-03-23 23:43:08 +0530

    added blueprint for basic search component

--- client/src/components/Header/Search/Search.jsx
+++ client/src/components/Header/Search/Search.jsx
@@ -1,6 +1,30 @@
+import { MdClose } from "react-icons/md";
+import prod from "../../../assets/products/earbuds-prod-1.webp";
+
 import "./Search.scss";
-const Search = () => {
-    return <div>Search</div>;
+
+const Search = ({ setShowSearch }) => {
+  return (
+    <div className="searchModal">
+      <div className="formField">
+        <input type="text" autoFocus placeholder="Search for products...." />
+        <MdClose onClick={() => setShowSearch(false)} />
+      </div>
+      <div className="searchResultContent">
+        <div className="searchResults">
+          <div className="searchResultItem">
+            <div className="imgContainer">
+              <img src={prod} alt="product" />
+            </div>
+            <div className="prodDetails">
+              <span className="name">Product Name</span>
+              <span className="desc">Product Desc</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
