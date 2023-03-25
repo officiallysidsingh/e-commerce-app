47a6b82bd970ec3b1c318119bdb47e144c42a418
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-03-25 22:39:15 +0530

    made product component responsive by mapping over image data, title data and price data of product state

--- client/src/components/Products/Product/Product.jsx
+++ client/src/components/Products/Product/Product.jsx
@@ -1,16 +1,19 @@
-import prod from "../../../assets/products/earbuds-prod-1.webp";
-
 import "./Product.scss";
 
-const Product = () => {
+const BASE_URL = import.meta.env.VITE_APP_DEV_URL;
+
+const Product = ({ data, id }) => {
   return (
     <div className="productCard">
       <div className="thumbnail">
-        <img src={prod} alt="Product Image" />
+        <img
+          src={BASE_URL + data.Img.data[0].attributes.url}
+          alt="Product Image"
+        />
       </div>
       <div className="productDetails">
-        <span className="name">Product Name</span>
-        <span className="price">&#8377;499</span>
+        <span className="name">{data.Title}</span>
+        <span className="price">&#8377;{data.Price}</span>
       </div>
     </div>
   );
