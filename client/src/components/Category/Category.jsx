6a60eb9a44ad3a2444609ccd8965d9433942479e
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-03-27 23:57:33 +0530

    made useFetch hook to fetch data

--- client/src/components/Category/Category.jsx
+++ client/src/components/Category/Category.jsx
@@ -1,8 +1,16 @@
-import Products from "../Products/Products";
+import { useParams } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
 
+import Products from "../Products/Products";
 import "./Category.scss";
 
 const Category = () => {
+  const { id } = useParams();
+
+  const { data } = useFetch(
+    `/api/products?populate=*&[filters][categories][id]=${id}`
+  );
+
   return (
     <div className="categoryMainContent">
       <div className="layout">
