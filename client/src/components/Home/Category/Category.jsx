88e0edf39e5ac090f41882f05b4b780d516451c3
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-03-27 23:47:20 +0530

    added category link to send to specific category page onclick

--- client/src/components/Home/Category/Category.jsx
+++ client/src/components/Home/Category/Category.jsx
@@ -1,13 +1,20 @@
+import { useNavigate } from "react-router-dom";
+
 import "./Category.scss";
 
 const BASE_URL = import.meta.env.VITE_APP_DEV_URL;
 
 const Category = ({ categories }) => {
+  const navigate = useNavigate();
   return (
     <div className="shopByCategory">
       <div className="categories">
         {categories?.data?.map((category) => (
-          <div key={category.id} className="category">
+          <div
+            key={category.id}
+            className="category"
+            onClick={() => navigate(`/category/${category.id}`)}
+          >
             <img
               src={BASE_URL + category.attributes.Img.data.attributes.url}
               alt="Category"
