e4762b8b53eab62de2b46c0e34f45dfd3875e35f
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-03-28 00:11:48 +0530

    added onclick event listener to send to product page on click of a product

--- client/src/components/Products/Product/Product.jsx
+++ client/src/components/Products/Product/Product.jsx
@@ -1,10 +1,13 @@
+import { useNavigate } from "react-router-dom";
+
 import "./Product.scss";
 
 const BASE_URL = import.meta.env.VITE_APP_DEV_URL;
 
 const Product = ({ data, id }) => {
+  const navigate = useNavigate();
   return (
-    <div className="productCard">
+    <div className="productCard" onClick={() => navigate("/product/" + id)}>
       <div className="thumbnail">
         <img
           src={BASE_URL + data.Img.data[0].attributes.url}
